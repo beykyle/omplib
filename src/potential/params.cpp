0eca161bffcb9241b7a6d21b1b4a9c078fde9a5d
beykyle <beykyle@umich.edu>
2022-08-22 19:41:49 -0400

    add constants

--- src/potential/params.cpp
+++ src/potential/params.cpp
@@ -9,7 +9,7 @@ double KoningDelaroche03<Projectile::proton>::real_coul_V(
     int Z, int A, double erg) const {
   const double z = static_cast<double>(Z);
   const double a = static_cast<double>(A);
-  return 6. * z * e * e / ( 5. * real_coul_r(z,a,erg) * pow(a,1./3.));
+  return 6. * z * e_sqr / ( 5. * real_coul_r(z,a,erg) * pow(a,1./3.));
 }
 
 double KoningDelaroche03<Projectile::proton>::real_coul_r(
---
+++ src/util/constants.hpp
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_HEADER
+#define CONSTANTS_HEADER
+
+namespace omplib {
+  constexpr static double e_sqr = 1.4399764; // square of elementary charge [ (Mev fm) ]
+};
+
+
+#endif
