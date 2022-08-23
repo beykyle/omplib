69263c595d49f88b9f63ffeb131a70bf98857f0d
beykyle <beykyle@umich.edu>
2022-08-23 15:46:43 -0400

    fully implemented KD03 and KDUQ OMPs

--- src/omplib.cpp
+++ src/omplib.cpp
@@ -2,19 +2,21 @@
 
 #include <iostream>
 
-using omplib::Projectile;
+using omplib::Proj;
 
 int main(int argc, char** argv) {
   std::cout << "Running test problem!\n" << std::flush;
   
-  auto kdn = omplib::KoningDelaroche03<Projectile::neutron>{};
-  auto kdp = omplib::KoningDelaroche03<Projectile::proton>{};
+  auto kdn_def = omplib::KoningDelaroche03<Proj::neutron>();
+  auto kdn_uq = omplib::KDUQ<Proj::neutron>();
+  auto kdp_def = omplib::KoningDelaroche03<Proj::proton>();
+  auto kdp_uq = omplib::KDUQ<Proj::proton>();
 
-  static_assert(kdn.projectile == Projectile::neutron);
-  static_assert(kdp.projectile == Projectile::proton);
+  static_assert(kdn_def.projectile == Proj::neutron);
+  static_assert(kdp_def.projectile == Proj::proton);
+  static_assert(kdn_uq.projectile == Proj::neutron);
+  static_assert(kdp_uq.projectile == Proj::proton);
 
-
-    
 
   return 0;
 };
