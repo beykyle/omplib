cb4adf08f35747ff07a67c18c4bfe4f317136cd3
beykyle <beykyle@umich.edu>
2022-10-21 12:06:51 -0600

    remove KDUQ class, replace w/ static factory fxn

--- src/omplib.cpp
+++ src/omplib.cpp
@@ -1,21 +1,24 @@
+#include "potential/kd_params.hpp"
 #include "potential/params.hpp"
 
 #include <iostream>
 
-using omplib::Proj;
+constexpr auto n = omplib::Proj::neutron;
+constexpr auto p = omplib::Proj::neutron;
 
 int main(int argc, char** argv) {
   std::cout << "Running test problem!\n" << std::flush;
   
-  auto kdn_def = omplib::KoningDelaroche03<Proj::neutron>();
-  auto kdn_uq = omplib::KDUQ<Proj::neutron>();
-  auto kdp_def = omplib::KoningDelaroche03<Proj::proton>();
-  auto kdp_uq = omplib::KDUQ<Proj::proton>();
+  auto kdn_def  = omplib::KoningDelaroche03<n>();
+  auto kdn_uq   = omplib::KoningDelaroche03<n>::build_KDUQ();
+  auto kdp_def  = omplib::KoningDelaroche03<p>();
+  auto kdp_uq   = omplib::KoningDelaroche03<p>::build_KDUQ();
 
-  static_assert(kdn_def.projectile == Proj::neutron);
-  static_assert(kdp_def.projectile == Proj::proton);
-  static_assert(kdn_uq.projectile == Proj::neutron);
-  static_assert(kdp_uq.projectile == Proj::proton);
+  static_assert(kdn_def.projectile == n);
+  static_assert(kdn_uq.projectile  == n);
+  
+  static_assert(kdp_def.projectile == p);
+  static_assert(kdp_uq.projectile  == p);
 
 
   return 0;
