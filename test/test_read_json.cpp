f0acd13733f575dc8666a460137f42a215a81138
beykyle <beykyle@umich.edu>
2022-10-20 18:50:26 -0600

    Add test for input parsing

--- test/test_read_json.cpp
+++ test/test_read_json.cpp
@@ -2,6 +2,23 @@
 #include "catch2/matchers/catch_matchers.hpp"
 #include "catch2/matchers/catch_matchers_floating_point.hpp"
 
-TEST_CASE("Read json params from file") {
+#include "potential/params.hpp"
 
+#include <fstream>
+
+
+using namespace omplib;
+
+TEST_CASE("Read json params from file") {
+  
+  // read default
+  const auto path  = "../../data/KDUQSamples/KDGlobal.json";
+  auto fstr  = std::ifstream(path);
+  json pfile = json::parse(fstr);
+  const auto kdn   = KoningDelaroche03<Proj::neutron>(pfile);
+
+  // built in default
+  const auto kdn_def = KoningDelaroche03<Proj::neutron>();
+
+  REQUIRE( kdn.real_cent_r(66,156,189.23 ) == kdn_def.real_cent_r(66,156,189.23));
 }
