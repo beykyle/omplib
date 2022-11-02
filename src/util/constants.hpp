0b13a393987c9743eb582df6349fbdd61f004ea8
beykyle <beykyle@umich.edu>
2022-11-02 17:41:05 -0600

    Add constants

--- src/util/constants.hpp
+++ src/util/constants.hpp
@@ -2,12 +2,36 @@
 #define CONSTANTS_HEADER
 
 namespace omplib {
+namespace constants {
   ///@brief square of elementary charge [ Mev fm ]
   constexpr static double e_sqr = 1.4399764;
   ///@brief pion mass [ Mev ]
   constexpr static double m_pi  = 139.57061;
+  /// @brief hbar [Mev s]
+  constexpr static double hbar = 6.58212196e-22;
+  /// @brief Coulomb's constant 
+  constexpr static double q_coulomb = 1.60217733e-19; 
+  /// @brief free-space permittivity [MeV fm / c^2 ]
+  constexpr static double eps_permitiviyty = 5.60958617e+37;
+  /// @brief zoom fast light speed [fm/s]
+  constexpr static double c = 2.99792458e+23;
+  
+  // dimensionless
+  /// @brief hbar^2/(m_pion * c)^2 
+  constexpr static double csp = 2.04553; 
   ///@brief bruh, it's pi
-  constexpr static double pi  = 3.1415926536;
+  constexpr static double pi    = 3.1415926536;
+  /// @brief fine structure
+  constexpr static double alpha = 7.2973525376e-03;
+
+  // [amu]
+  constexpr static double n_mass_amu = 1.00866491578;
+  constexpr static double e_mass_amu = 5.48579911e-4;
+  constexpr static double p_mass_amu = 1.00727646688;
+
+  constexpr static double ev_per_amu  = 9.31494013e+8; 
+  constexpr static double MeV_per_amu = 931.4943335;
+};
 };
 
 
