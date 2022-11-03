877d66be31281ce30f35833186f60687238731bb
beykyle <beykyle@umich.edu>
2022-11-03 15:36:08 -0600

    Add NonLocal potentials

--- src/potential/potential.hpp
+++ src/potential/potential.hpp
@@ -10,14 +10,28 @@
 
 namespace omplib {
 
-///@brief Base class for an instance of a potential between
+///@brief Base class for an instance of a local potential between
 /// some target (A,Z) and a projectile at a given cms energy
 struct Potential {
   using ptr = std::unique_ptr<Potential>;
   using cptr = std::unique_ptr<const Potential>;
 
+  /// @brief r is the distance between the projectile and target
   virtual std::complex<double> eval(double r, double erg) const = 0;
   std::complex<double> operator () (double r, double erg) const { return eval(r, erg); }
+};
+
+///@brief Base class for an instance of a non-local potential between
+/// some target (A,Z) and a projectile at a given cms energy
+struct NonLocalPotential {
+  using ptr = std::unique_ptr<NonLocalPotential>;
+  using cptr = std::unique_ptr<const NonLocalPotential>;
+
+  /// @brief r is a distance between the projectile and target
+  /// @brief rp is another distance between the projectile and target
+  virtual std::complex<double> eval(double r, double rp, double erg) const = 0;
+  std::complex<double> operator () (double r, double rp, double erg) const { 
+    return eval(r,rp, erg); }
 };
 
 /// @brief Common phenomenological potential form used for central potentials
@@ -30,6 +44,10 @@ public:
     : V(V), R(R), a(a) {};
   WoodsSaxon(std::complex<double> V, double R, double a)
     : V(V), R(R), a(a) {};
+  
+  WoodsSaxon(const WoodsSaxon& rhs) = default;
+  WoodsSaxon(WoodsSaxon&& rhs) = default;
+  WoodsSaxon() = default;
   
   std::complex<double> eval(double r, double erg) const final {
     return V/(1. + exp((r-R)/a));
@@ -48,6 +66,10 @@ public:
   DerivWoodsSaxon(std::complex<double> V, double R, double a)
     : V(V), R(R), a(a) {};
   
+  DerivWoodsSaxon(const DerivWoodsSaxon& rhs) = default;
+  DerivWoodsSaxon(DerivWoodsSaxon&& rhs) = default;
+  DerivWoodsSaxon() = default;
+  
   std::complex<double> eval(double r, double erg) const final {
     const auto y = exp((r-R))/a;
     return - V / a * ( y / ( (1. + y) * (1+y) ));
@@ -56,7 +78,7 @@ public:
 
 /// @brief Common phenomenological potential form used for spin orbit potentials
 /// the derivative in r of a Woods-Saxon, times 1/r
-class Thomas : Potential {
+class Thomas : public Potential {
 private:
   std::complex<double> V;
   double R, a;
@@ -66,12 +88,16 @@ public:
   Thomas(std::complex<double> V, double R, double a)
     : V(V), R(R), a(a) {};
   
+  Thomas(const Thomas& rhs) = default;
+  Thomas(Thomas&& rhs) = default;
+  Thomas() = default;
+  
   std::complex<double> eval(double r, double erg) const final {
     return DerivWoodsSaxon(V,R,a).eval(r,erg)/r;
   };
 };
 
-class Gaussian : Potential {
+class Gaussian : public Potential {
 private:
   std::complex<double> V;
   double R, sigma;
@@ -81,13 +107,17 @@ public:
   Gaussian(std::complex<double> V, double R, double sigma)
     : V(V), R(R), sigma(sigma) {};
   
+  Gaussian(const Gaussian& rhs) = default;
+  Gaussian(Gaussian&& rhs) = default;
+  Gaussian() = default;
+  
   std::complex<double> eval(double r, double erg) const final {
     return exp( (r-R)*(r-R)/(sigma*sigma) );
   };
 };
 
 template<size_t N>
-class NGaussian : Potential {
+class NGaussian : public Potential {
 private:
   std::array<Gaussian,N> gaussians;
 
@@ -95,19 +125,44 @@ public:
   NGaussian(std::array<Gaussian,N> gaussians):
     gaussians(gaussians) {};
   
+  NGaussian(const NGaussian& rhs) = default;
+  NGaussian(NGaussian&& rhs) = default;
+  
+  
   std::complex<double> eval(double r, double erg) const final {
-    double v = 0;
+    std::complex<double> v = 0;
     for (int i = 0; i < N; ++i) {
-      v += gaussians[i].eval(r);
+      v += gaussians[i].eval(r, erg);
     }
+    return v;
   };
 };
 
 using DoubleGaussian = NGaussian<2>;
 
+///@brief Thompson, D. R., LeMere, M., & Tang, Y. C. (1977). 
+/// Systematic investigation of scattering problems with the resonating-group method. 
+/// Nuclear Physics A, 286(1), 53-66.
+class Minnesota : public DoubleGaussian {
+public:
+  static Minnesota build_1S0() {
+    return Minnesota(200, -98.15, 1.487, 0.465);
+  }
+  static Minnesota build_3S1() {
+    return Minnesota(200, -178, 1.487, 0.639);
+  }
+  
+  Minnesota(const Minnesota& rhs) = default;
+  Minnesota(Minnesota&& rhs) = default;
+
+  Minnesota(double V01, double V02, double k1, double k2):
+    DoubleGaussian({Gaussian(V01, 0., 1/sqrt(k1) ), Gaussian(V02, 0., 1/sqrt(k2) )}) {}
+};
+
+
 /// @brief Common global phenomenological OMP form for a given isotope, A,Z.
 template<class Params>
-class OMP : Potential {
+class OMP : public Potential {
 private:
   /// @brief Determines the parameterization used for calculating 
   /// term depths, radii, and diffusivities
@@ -121,6 +176,9 @@ public:
     : A(A), Z(Z), params(params) {};
   OMP(int A, int Z, Params&& params)
     : A(A), Z(Z), params(params) {};
+
+  OMP(const OMP<Params>& rhs) = default;
+  OMP(OMP<Params>&& rhs) = default;
 
   void reset_target(int An, int Zn) {
     A = An;
@@ -157,6 +215,25 @@ public:
 
     return V.eval(r,erg) + Vs.eval(r,erg) + Vso.eval(r,erg)  // Re
          + W.eval(r,erg) + Ws.eval(r,erg) + Wso.eval(r,erg); // Im
+  };
+};
+
+/// @brief An arbitrary local potential in r smeared into the off-diagonal 
+/// by a Gaussian factor in (r-rp), from:
+/// Perey, F., and B. Buck. 
+/// "A non-local potential model for the scattering of neutrons by nuclei." 
+/// Nuclear Physics 32 (1962): 353-380.
+class PereyBuck : public NonLocalPotential {
+private:
+  Potential::cptr local_potential;
+  Gaussian        non_local_factor;
+public:
+  PereyBuck(Potential::cptr&& potential, double beta)
+    : local_potential(std::move(potential))
+    , non_local_factor(1/(pow(constants::pi, 3./2.)) * beta*beta*beta, 0, beta) {}
+
+  std::complex<double> eval(double r, double rp, double erg) const final {
+    return local_potential->eval(r, erg) * non_local_factor( (r-rp), erg);
   };
 };
 
