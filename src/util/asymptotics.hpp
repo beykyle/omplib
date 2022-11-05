687aae616522dc2b5136075dc7d0fdbc3cfa65e4
Kyle Andrew Beyer <kbeyer@corson.lanl.gov>
2022-11-05 13:24:56 -0600

    real input, derivatives

--- src/util/asymptotics.hpp
+++ src/util/asymptotics.hpp
@@ -1,6 +1,6 @@
 
-#ifndef RMATRIX_HEADER
-#define RMATRIX_HEADER
+#ifndef ASYMPTOTICS_HEADER
+#define ASYMPTOTICS_HEADER
 
 #include <cmath>
 #include <complex>
@@ -8,77 +8,42 @@
 #include "util/constants.hpp"
 
 namespace omplib {
+
 ///@brief Spherical Bessel function of 1st kind 
 struct j {
   int l;
   j(int l) : l(l) {};
-  std::complex<double> operator () (std::complex<double> kr) {
+  std::complex<double> operator () (double kr) {
     return std::sph_bessel(l,kr);
   }
 };
 
-///@brief Spherical Bessel function of 2nd kind 
+///@brief Spherical Bessel function double
 struct eta {
   int l;
   eta(int l) : l(l) {};
-  std::complex<double> operator () (std::complex<double> kr) {
+  std::complex<double> operator () (double kr) {
     return std::sph_neumann(l,kr);
   }
 };
 
-///@brief Spherical Hankel function of 1st kind 
+///@brief Spherical Hankel function double
 struct h_out {
   int l;
   h_out(int l) : l(l) {};
-  std::complex<double> operator () (std::complex<double> kr) {
+  std::complex<double> operator () (double kr) {
     using constants::i;
     return j{l}(kr) + i * eta{l}(kr);
   }
 };
 
-///@brief Spherical Hankel function of 2nd kind 
+///@brief Spherical Hankel function double
 struct h_in {
   int l;
   h_in(int l) : l(l) {};
-  std::complex<double> operator () (std::complex<double> kr) {
+  std::complex<double> operator () (double kr) {
     using constants::i;
     return j{l}(kr) - i * eta{l}(kr);
-  }
-};
-
-///@brief derivative of spherical Bessel function of 1st kind 
-struct jp {
-  int l;
-  jp(int l) : l(l) {};
-  std::complex<std::complex<std::complex<double>>> operator () (std::complex<double> kr) {
-    //TODO
-  }
-};
-
-///@brief derivative of spherical Bessel function of 2nd kind 
-struct etap {
-  int l;
-  etap(int l) : l(l) {};
-  std::complex<std::complex<std::complex<double>>> operator () (std::complex<double> kr) {
-    //TODO
-  }
-};
-
-///@brief derivative of spherical Hankel function of 1st kind 
-struct hp_out {
-  int l;
-  hp_out(int l) : l(l) {};
-  std::complex<std::complex<std::complex<double>>> operator () (std::complex<double> kr) {
-    //TODO
-  }
-};
-
-///@brief derivative of spherical Hankel function of 2nd kind 
-struct hp_in {
-  int l;
-  hp_in(int l) : l(l) {};
-  std::complex<std::complex<std::complex<double>>> operator () (std::complex<double> kr) {
-    //TODO
   }
 };
 
