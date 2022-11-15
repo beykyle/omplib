7775b4ac19f1665fe9b9ced45cba74e5d2be1082
beykyle <beykyle@umich.edu>
2022-11-15 12:51:06 -0700

    add comments

--- src/util/config.hpp
+++ src/util/config.hpp
@@ -4,9 +4,12 @@
 #include "util/types.hpp"
 
 namespace omplib {
+  /// @brief  The number of basis functions to use in R-Matrix calculations
   constexpr int NBASIS  = 20;
+  /// @brief The maximum number of orbital angular momentum values to consider
+  /// for a single channel
   constexpr int MAXL    = 20;
-  
+  /// @brief Relative error between iterations indicating convergence
   constexpr real REL_ERR_EPS  = 1.0e-8;
 }
 
